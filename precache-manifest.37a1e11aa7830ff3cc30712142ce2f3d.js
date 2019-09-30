b37c8e52a5eccef1e9bd081a0ed4e39d81f5592d
Carter Cromer <webmaster.spd.calpoly@gmail.com>
2019-09-30 15:38:49 -0700

    Updates

---
+++ precache-manifest.37a1e11aa7830ff3cc30712142ce2f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e412b04f46e3f4e10fbc9487958aee",
+    "url": "/pledge/index.html"
+  },
+  {
+    "revision": "a47c869cfeabc48128c2",
+    "url": "/pledge/static/css/main.eb99b318.chunk.css"
+  },
+  {
+    "revision": "1f31a0382552b927bfa9",
+    "url": "/pledge/static/js/2.a79ca60e.chunk.js"
+  },
+  {
+    "revision": "a47c869cfeabc48128c2",
+    "url": "/pledge/static/js/main.c41ce596.chunk.js"
+  },
+  {
+    "revision": "16cf8e09f437947d6157",
+    "url": "/pledge/static/js/runtime-main.3daffb55.js"
+  },
+  {
+    "revision": "466e973058a65167120d387a9f7586ec",
+    "url": "/pledge/static/media/bg.466e9730.jpg"
+  },
+  {
+    "revision": "13f558790747468920f6eefe8d26a7c7",
+    "url": "/pledge/static/media/spdpic.13f55879.jpg"
+  }
+]);
