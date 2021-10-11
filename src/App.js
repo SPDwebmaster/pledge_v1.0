feeb7845d71b76ab5db473f07ab5c340b8121a9c
cartcrom <cartergcromer@gmail.com>
2021-10-11 13:06:07 -0700

    API workaround

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bg from './bg.jpg';
 import pic from './spdpic.jpg'
 import './App.css';
-import datastore from './datastore';
 import DashBoard from './components/dashboard';
 
 function App() {
+
+  const [pledgeClassName, setPledgeClassName] = useState("Pledge Class");
+
   return (
     <div className="App">
       <img className="bg" src={bg} alt="pic"/>
       <header className="App-header">
         <div className="frame">
           <img className="pic" src={pic} alt="pic"/>
-          <h2>Lambda Pledge Class</h2>
-          <DashBoard/>
+          <h2>{pledgeClassName}</h2>
+          <DashBoard setPledgeClassName={setPledgeClassName}/>
         </div>
       </header>
     </div>
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import autobind from 'class-autobind';
+import { readRemoteFile } from 'react-papaparse';
 import DataStore from '../datastore'
 import Marks from './marks'
 import './dashboard.css'
+import axios from 'axios'
 
 export default class DashBoard extends React.Component {
     constructor(props) {
@@ -12,10 +14,53 @@ export default class DashBoard extends React.Component {
         this.getData();
     }
 
+    setData(pledges) {
+        this.setState({pc: pledges})
+    }
+
     async getData() {
-        let pledge_class = await DataStore.getMarks();
-        await DataStore.getPledges();
-        this.setState({pc: pledge_class});
+        readRemoteFile("https://docs.google.com/spreadsheets/d/e/2PACX-1vRiZAFMM0rkejqhWxGIwuW___MGrYHmmIjw57v1pubLIMiJtaGY6iLvfCTMMkHiabF1Qs5j-k_R4dFt/pub?gid=1605365040&single=true&output=csv", {
+            complete: (results) => {
+                let pledge_data = results.data[0]
+                let pledge_class_name = pledge_data[0]
+                
+                this.props.setPledgeClassName(pledge_class_name + " Pledge Class")
+
+                let pledges = []
+                let pledge_dict = {}
+                
+                for (let i = 2; i < pledge_data.length; i++) {
+                    let pledge = new Pledge(pledge_data[i])
+                    pledge_dict[pledge.name] = pledge
+                    pledges.push(pledge)
+                }
+
+                
+                readRemoteFile('https://docs.google.com/spreadsheets/d/e/2PACX-1vRiZAFMM0rkejqhWxGIwuW___MGrYHmmIjw57v1pubLIMiJtaGY6iLvfCTMMkHiabF1Qs5j-k_R4dFt/pub?output=csv', {
+                    complete: (results) => {
+
+                        for (let i = 1; i < results.data.length; i++) {
+                            let x = results.data[i]
+                            let pledge_name = x[3]
+                            if (pledge_name in pledge_dict) {
+                                if (x[7] == "yes")
+                                    pledge_dict[x[3]].marks.push({date: x[0], brother: x[2], pledge: x[3], type: x[4], count: parseInt(x[5]), desc: x[6], approved: x[7]})
+                            } else {
+                                console.log("Unknown pledge: " + pledge_name)
+                            }
+                        }
+                        this.setData(pledges)
+                        return
+                    },
+                });
+
+
+            },
+          });
+      
+        //let pledge_class = await DataStore.getMarks();
+        //await DataStore.getPledges();
+        //this.setState({pc: pledge_class});
     }
 
     openPledge(pledge) {
@@ -39,11 +84,13 @@ export default class DashBoard extends React.Component {
                 <tr key={m.desc} className="details">
                     <td><Marks type={m.type} count={m.count}/></td>
                     <td className="details">{m.date}</td>
-                    <td className="details">{m.desc}</td>
+                    <td className="details">{m.brother + ' - ' + m.desc}</td>
                 </tr>)
             });
             return details;
         }
+
+        
 
         let Pledge = (pledge) => {
             return(
@@ -58,11 +105,9 @@ export default class DashBoard extends React.Component {
         pc.forEach(p => {
             pledges.push(Pledge(p));
             if (open === p.name) {
-                console.log(p.name);
                 pledges = pledges.concat(MarkDetails(p.marks));
             }
         })
-        console.log(pledges);
             
         return(
             <div className="dash">
@@ -85,5 +130,34 @@ export default class DashBoard extends React.Component {
                 <br></br>
             </div>
         )
+    }
+}
+
+class Mark {
+    constructor(date, brother, pledge, type, count, desc, approved) {
+    this.date = date
+    this.brother = brother
+    this.pledge = pledge
+    this.type = type
+    this.count = count
+    this.desc = desc
+    this.approved = approved
+    }
+}
+
+class Pledge {
+    constructor(name) {
+        this.name = name;
+        this.marks = [];
+    }
+
+    getTotal(type) {
+        let total = 0;
+        for (let i in this.marks) {
+            if (this.marks[i].type === type)
+                total += this.marks[i].count;
+        }
+
+        return total;
     }
 }
