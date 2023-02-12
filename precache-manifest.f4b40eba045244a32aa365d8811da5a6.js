d93a04e2aac28eb4b20ef2732cd83643d3d651a2
Casey Avila <casey@theavilas.org>
2023-02-12 11:01:18 -0800

    Updates

---
+++ precache-manifest.f4b40eba045244a32aa365d8811da5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fe2da626dd2fe7e1491b0f68f7ead7",
+    "url": "/pledge/index.html"
+  },
+  {
+    "revision": "2e27d29d636801c4945b",
+    "url": "/pledge/static/css/main.845a5217.chunk.css"
+  },
+  {
+    "revision": "97755c258e39b4a344a4",
+    "url": "/pledge/static/js/2.729a679d.chunk.js"
+  },
+  {
+    "revision": "51ebe7d5c0448f22b69f97ee43c9113a",
+    "url": "/pledge/static/js/2.729a679d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e27d29d636801c4945b",
+    "url": "/pledge/static/js/main.bb43a34e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/pledge/static/js/main.bb43a34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec0e5b277b85e3c4562",
+    "url": "/pledge/static/js/runtime-main.5b4d0579.js"
+  }
+]);
